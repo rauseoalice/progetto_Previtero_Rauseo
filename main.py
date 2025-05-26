0a8ac1f93d961301c2e0c0a284f45e9f681a62c7
ALICE RAUSEO <rauseoalice@gmail.com>
2025-05-26 17:09:19 +0200

    creato inserimento dati standard + creato summary

--- main.py
+++ main.py
@@ -24,6 +24,65 @@ def index():
 
 @app.route('/dati/<dato>',methods=['GET'])
 def read(dato):
+    if dato == 'station':
+        entity = db.collection('dati').document(dato).get()
+        if entity.exists:
+            d = entity.to_dict()
+            return json.dumps(d['readings']), 200
+        else:
+            return 'not found', 404
+    elif dato == 'item':
+        entity = db.collection('dati').document(dato).get()
+        if entity.exists:
+            d = entity.to_dict()
+            return json.dumps(d['readings']), 200
+        else:
+            return 'not found', 404
+    else:
+        return 'not found', 404
+
+
+@app.route('/dati/<dato>', methods=['POST'])
+def new_data(dato):
+    if dato == 'station':
+        code = request.values['code']
+        namedist = request.values['namedist']
+        address = request.values['address']
+        latitude = request.values['latitude']
+        longitude = request.values['longitude']
+
+        entity = db.collection('dati').document(dato).get()
+        if entity.exists:
+            d = entity.to_dict()
+            d['readings'].append({'code': code, 'namedist': namedist, 'address': address, 'latitude': latitude, 'longitude': longitude})
+            db.collection('dati').document(dato).set(d)
+        else:
+            db.collection('dati').document(dato).set({'readings':[{'code': code, 'namedist': namedist, 'address': address, 'latitude': latitude, 'longitude': longitude}]})
+        return 'ok', 200
+    elif dato == 'item':
+        item_code = request.values['item_code']
+        item_name = request.values['item_name']
+        unit = request.values['unit']
+        good = request.values['good']
+        normal = request.values['normal']
+        bad = request.values['bad']
+        very_bad = request.values['very_bad']
+
+        entity = db.collection('dati').document(dato).get()
+        if entity.exists:
+            d = entity.to_dict()
+            d['readings'].append({'item_code': item_code, 'item_name': item_name, 'unit': unit, 'good': good, 'normal': normal, 'bad': bad, 'very_bad': very_bad})
+            db.collection('dati').document(dato).set(d)
+        else:
+            db.collection('dati').document(dato).set({'readings':[{'item_code': item_code, 'item_name': item_name, 'unit': unit, 'good': good, 'normal': normal, 'bad': bad, 'very_bad': very_bad}]})
+        return 'ok', 200
+    else:
+        return 'not found', 404
+
+
+
+@app.route('/valori/<dato>', methods=['GET'])
+def read_values(dato):
     entity = db.collection('dati').document(dato).get()
     if entity.exists:
         d = entity.to_dict()
@@ -33,21 +92,31 @@ def read(dato):
 
 
 
-@app.route('/dati/<dato>', methods=['POST'])
-def new_data(dato):
+@app.route('/valori/<dato>', methods=['POST'])
+def new_values(dato):
+    data = request.values['data']
     code = request.values['code']
-    namedist = request.values['namedist']
     address = request.values['address']
     latitude = request.values['latitude']
     longitude = request.values['longitude']
+    SO2 = request.values['SO2']
+    NO2 = request.values['NO2']
+    O3 = request.values['O3']
+    CO = request.values['CO']
+    PM10 = request.values['PM10']
+    PM25 = request.values['PM25']
 
     entity = db.collection('dati').document(dato).get()
     if entity.exists:
         d = entity.to_dict()
-        d['readings'].append({'code': code, 'namedist': namedist, 'address': address, 'latitude': latitude, 'longitude': longitude})
+        d['readings'].append({'data': data, 'code': code, 'address': address, 'latitude': latitude, 'longitude': longitude,
+                                  'SO2': SO2, 'NO2': NO2, 'O3': O3, 'CO': CO, 'PM10': PM10, 'PM25': PM25})
         db.collection('dati').document(dato).set(d)
     else:
-        db.collection('dati').document(dato).set({'readings':[{'code': code, 'namedist': namedist, 'address': address, 'latitude': latitude, 'longitude': longitude}]})
+        db.collection('dati').document(dato).set({'readings':[{'data': data, 'code': code, 'address': address, 
+                                                                   'latitude': latitude, 'longitude': longitude,
+                                                                   'SO2': SO2, 'NO2': NO2, 'O3': O3, 
+                                                                   'CO': CO, 'PM10': PM10, 'PM25': PM25}]})
     return 'ok', 200
 
 
--- station.py
+++ station.py
@@ -10,22 +10,51 @@ name = 'station'
 with open('Measurement_station_info.csv') as f:
     next(f)
     for line in f:
-        for line in f:
-            line = line.strip()
+        line = line.strip()
         if line:
             parts = line.split('"')
-            if len(parts) >= 5:
-                first_part = parts[0].rstrip(',')
-                second_part = parts[1]
-                third_part = parts[3]
-                
-                code, namedist = first_part.strip.split(',', 1)
-                address = second_part
-                latitude, longitude = third_part.split(',', 1)
-                
-                post(f'{server}/dati/{name}', json={
-                    'code': int(code), 'namedist': namedist, 'address': address,
-                    'latitude': float(latitude), 'longitude': float(longitude)
-                    })
-                print(code, namedist, address, latitude, longitude)
+            first_part = parts[0].rstrip(',')
+            second_part = parts[1]
+            third_part = parts[2]
+            third_part_good = third_part.strip(',')
+                    
+            code, namedist = first_part.split(',', 1)
+            address = second_part
+            latitude, longitude = third_part_good.split(',',1)
+                    
+            post(f'{server}/dati/{name}', data={
+                        'code': int(code), 'namedist': namedist, 'address': address,
+                        'latitude': float(latitude), 'longitude': float(longitude)
+                })
+                    
+            print(code, namedist, address, latitude, longitude)
 
+
+
+name = 'item'
+
+with open('Measurement_item_info.csv') as f:
+    next(f)
+    for line in f:
+        line = line.strip()
+        if line:
+            parts = line.split(',')
+            item_code = int(parts[0])
+            item_name = parts[1]
+            unit = parts[2]
+            good = float(parts[3])
+            normal = float(parts[4])
+            bad = float(parts[5])
+            very_bad = float(parts[6])
+            
+            post(f'{server}/dati/{name}', data={
+                'item_code': item_code,
+                'item_name': item_name,
+                'unit': unit,
+                'good': good,
+                'normal': normal,
+                'bad': bad,
+                'very_bad': very_bad
+            })
+            
+            print(item_code, item_name, unit, good, normal, bad, very_bad)
